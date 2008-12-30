ac8f93f23bfc7200f5d13b616b55405ece98b233
Benjamin Berg <benjamin@sipsolutions.net>
2008-12-30 20:25:10 +0100

    Initial commit.

---
+++ test.c
@@ -0,0 +1,17 @@
+#include "SHA3api_ref.h"
+#include <stdio.h>
+#include <string.h>
+
+char *data = "blubasdflkjasldkfjlkasjdflkajsdflkjasldkfjd";
+
+int main()
+{
+  unsigned char hash[512/8];
+  int i;
+  Hash(512, data, strlen(data)*8, &hash);
+  for (i = 0; i < 512/8; i++) {
+    printf ("%02x", hash[i]);
+  }
+  printf("\n");
+  return 0;
+}
