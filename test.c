66ab22c21f325623b592141fd86897f7988a5180
Benjamin Berg <benjamin@sipsolutions.net>
2009-01-01 18:47:05 +0100

    Added 256 bit tests.

--- test.c
+++ test.c
@@ -8,22 +8,39 @@
 static struct {
 	unsigned char *data, *result;
 	size_t datalen;
-} tests[] = {
-#include "short_test_data.h"
-#include "long_test_data.h"
+} tests512[] = {
+#include "short_test_512_data.h"
+#include "long_test_512_data.h"
 };
 
-#define NT (sizeof(tests)/sizeof(tests[0]))
+static struct {
+	unsigned char *data, *result;
+	size_t datalen;
+} tests256[] = {
+#include "short_test_256_data.h"
+#include "long_test_256_data.h"
+};
+
+#define ITEMS(array) (sizeof(array)/sizeof(array[0]))
 
 int main(void)
 {
-	BitSequence hash[512 / 8];
+	BitSequence hash256[256 / 8];
+	BitSequence hash512[512 / 8];
 	int i;
-	for (i = 0; i < NT; i++) {
-		Hash(512, tests[i].data, tests[i].datalen, hash);
-		printf("nbits = %d\n", tests[i].datalen);
-		if (memcmp(tests[i].result, hash, sizeof(hash)))
+	for (i = 0; i < ITEMS(tests256); i++) {
+		Hash(256, tests256[i].data, tests256[i].datalen, hash256);
+		printf("256 nbits = %d\n", tests256[i].datalen);
+		if (memcmp(tests256[i].result, hash256, sizeof(hash256)))
 		        printf("FAIL %d!\n", i);
 	}
+
+	for (i = 0; i < ITEMS(tests512); i++) {
+		Hash(512, tests512[i].data, tests512[i].datalen, hash512);
+		printf("512 nbits = %d\n", tests512[i].datalen);
+		if (memcmp(tests512[i].result, hash512, sizeof(hash512)))
+		        printf("FAIL %d!\n", i);
+	}
+
 	return 0;
 }
