ad73ce2f28ea733ca879254265fa5558d11b3e8d
Benjamin Berg <benjamin@sipsolutions.net>
2009-01-01 19:03:39 +0100

    Make test only print failures. Also return 1 if a test failed.

--- test.c
+++ test.c
@@ -28,19 +28,23 @@ int main(void)
 	BitSequence hash256[256 / 8];
 	BitSequence hash512[512 / 8];
 	int i;
+	int result = 0;
+
 	for (i = 0; i < ITEMS(tests256); i++) {
 		Hash(256, tests256[i].data, tests256[i].datalen, hash256);
-		printf("256 nbits = %d\n", tests256[i].datalen);
-		if (memcmp(tests256[i].result, hash256, sizeof(hash256)))
-		        printf("FAIL %d!\n", i);
+		if (memcmp(tests256[i].result, hash256, sizeof(hash256))) {
+		        printf("FAIL 256bit: %d!\n", i);
+		        result = 1;
+                }
 	}
 
 	for (i = 0; i < ITEMS(tests512); i++) {
 		Hash(512, tests512[i].data, tests512[i].datalen, hash512);
-		printf("512 nbits = %d\n", tests512[i].datalen);
-		if (memcmp(tests512[i].result, hash512, sizeof(hash512)))
-		        printf("FAIL %d!\n", i);
+		if (memcmp(tests512[i].result, hash512, sizeof(hash512))) {
+		        printf("FAIL 512bit: %d!\n", i);
+                        result = 1;
+                }
 	}
 
-	return 0;
+	return result;
 }
